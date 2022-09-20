838b7d26a8a49fc44c7544bfe61f36ae3ae7b154
Cris M <cristian.montenegro.p@gmail.com>
2022-09-20 20:19:12 -0300

    added index.js for ES6 Module usage

--- moduleFn/index.js
+++ moduleFn/index.js
@@ -1,3 +1,6 @@
 export { indexFile } from './htmlFileIndexer.js';
-export { fileToEdit } from '../insert-file-here/file.js';
+// export { fileToEdit } from '../insert-file-here/file.js';
 export { replaceElement } from './elementReplacer.js';
+export { writeOnFile } from './writeOnFile.js';
+export { readFile } from './readFile.js';
+// export { esitedFile } from './edited-file/hello.js';
