d782a41576337b4fc734d3eb330f006f2860b463
Cris M <cristian.montenegro.p@gmail.com>
2022-10-04 22:48:37 -0300

    separated functionality into two, readFiles (read all files in Dir) and readFileContent (read actual content of specific file)

--- moduleFn/readFile.js
+++ moduleFn/readFile.js
@@ -1,26 +1,23 @@
 import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import util from 'util';
-const __filename = fileURLToPath(import.meta.url);
-// console.log(__filename);
-const __dirname = path.dirname(__filename);
-// console.log(__dirname);
-let dirPath = path.join(__dirname, '../insert-file-here');
 
-export async function readFile() {
+// Function that reads file content of a Directory, input is directory's absolute path, output is array of file names.
+export async function readFiles(inputDirPath) {
   const readDirPromise = util.promisify(fs.readdir);
+
+  const filesInDir = await readDirPromise(inputDirPath).catch((err) =>
+    console.log('Error!', err)
+  );
+  return filesInDir;
+}
+
+// Function that reads content of a file, input is file's absolute path, out put is file's content
+export async function readFileContent(inputFilePath) {
   const readFilePromise = util.promisify(fs.readFile);
 
-  const filesInDir = await readDirPromise(dirPath).catch((err) =>
-    console.log('Error!', err)
+  const fileContent = await readFilePromise(inputFilePath, 'utf8').catch(
+    (err) => console.log('Error!', err)
   );
-  let filePath = path.join(dirPath, filesInDir[0]);
-  const fileContent = await readFilePromise(filePath, 'utf8').catch((err) =>
-    console.log('Error!', err)
-  );
-  // console.log('filePath: ', filePath);
-  // console.log('fileName: ', filesInDir);
-  // console.log('file Content: ', fileContent);
+
   return fileContent;
 }
