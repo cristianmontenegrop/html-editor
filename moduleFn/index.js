83bb133892f62cbb4259a4fdf1f411b3471d4ee9
Cris M <cristian.montenegro.p@gmail.com>
2022-10-04 23:01:03 -0300

    modified file in order to accomodate changes to function structure in readFile.js

--- moduleFn/index.js
+++ moduleFn/index.js
@@ -2,5 +2,5 @@ export { indexFile } from './htmlFileIndexer.js';
 // export { fileToEdit } from '../insert-file-here/file.js';
 export { replaceElement } from './elementReplacer.js';
 export { writeOnFile } from './writeOnFile.js';
-export { readFile } from './readFile.js';
+export { readFiles, readFileContent } from './readFile.js';
 // export { esitedFile } from './edited-file/hello.js';
