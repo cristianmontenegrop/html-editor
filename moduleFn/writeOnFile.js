0cc509d8c9ddd768da9477ee9f8b9395d571c0dd
Cris M <cristian.montenegro.p@gmail.com>
2022-10-05 15:08:15 -0300

    all the main functions of writeOnFile are functional, still missing final operation before returning

--- moduleFn/writeOnFile.js
+++ moduleFn/writeOnFile.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import util from 'util';
 import inquirer from 'inquirer';
+import path from 'path';
 
 // export async function writeOnFile(outputDirPath, editedFileContent) {
 export async function writeOnFile(fileInfo) {
@@ -13,7 +14,7 @@ export async function writeOnFile(fileInfo) {
   const fsUnlinkPromise = promisify(fs.unlink);
   const fsAppendPromise = promisify(fs.appendFile);
 
-  let writeOnFileObj = {
+  let fsFnObj = {
     access: function () {
       console.log('access!');
       let fileExists = fs.existsSync(fileInfo.outputFilePath);
@@ -21,42 +22,77 @@ export async function writeOnFile(fileInfo) {
     },
     copy: async function () {
       console.log('copy!');
+
+      const pathBegining =
+        fileInfo.outputRelativeFilePath.split(/\.[0-9a-z]+$/i);
+      const pathEnd = fileInfo.outputRelativeFilePath.split(/^.+\./i);
+      const copyPath = pathBegining[0] + 'copy.' + pathEnd[pathEnd.length - 1];
+      console.log('copyPath: ', copyPath);
       return await fsCopyPromise(
         fileInfo.outputRelativeFilePath,
-        fileInfo.outputRelativeDirPath
+        copyPath,
+        fs.constants.COPYFILE_FICLONE
       );
     },
-    unLink: async function () {
+    unLink: async function (path) {
       console.log('unlink!');
-      return await fsUnlinkPromise(fileInfo.outputRelativeFilePath).catch(
-        (err) => console.log('Error!', err)
-      );
+      try {
+        return await fsUnlinkPromise(path);
+      } catch (err) {
+        console.log('Error!', err);
+      }
     },
     append: async function () {
       console.log('append!');
-      return await fsAppendPromise(
+      const res = await fsAppendPromise(
         fileInfo.outputRelativeFilePath,
         fileInfo.editedFileContent
       ).catch((err) => console.log('Error!', err));
+      return res;
     },
   };
 
-  let x = writeOnFileObj.access();
+  let x = fsFnObj.access();
+  console.log('x: ', x);
+  if (x) {
+    await inquirer
+      .prompt([
+        {
+          name: 'confirmCopy',
+          type: 'confirm',
+          message:
+            'Warning!, existing file in output folder with same name, create copy?',
+        },
+      ])
+      .then(async ({ confirmCopy }) => {
+        let copyRes;
+        if (confirmCopy) {
+          copyRes = await fsFnObj.copy();
+          console.log('copyRes: ', copyRes);
+        }
+        if (copyRes === undefined) {
+          const fileUnlink = await fsFnObj.unLink(
+            fileInfo.outputRelativeFilePath
+          );
+          console.log('fileUnlink: ', fileUnlink);
+        }
+        
 
-  // inquirer
-  //   .prompt([
-  //     {
-  //       name: 'confirmCopy',
-  //       type: 'confirm',
-  //       message:
-  //         'Warning!, existing file in output folder with same name, create copy?',
-  //     },
-  //   ])
-  //   .then(({ confirmCopy }) => {
-  //     if (confirmCopy) {
-  //       writeOnFileObj.copy(copyFile)
-  //       console.log('copyFile: ', copyFile);
-  //     }
+      });
+  } else {
+    try {
+      console.log('try statement exec!: ');
+
+      const appendFile = await fsFnObj.append();
+      if (appendFile === undefined) {
+        return fsFnObj.access(fileInfo.outputRelativeFilePath);
+      } else {
+        return false;
+      }
+    } catch {
+      (err) => console.log('Error!', err);
+    }
+  }
 
   // await fsAccesPromise(fileInfo.outputFilePath)
   //   .then((x) => {
