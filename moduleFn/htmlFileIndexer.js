8db2ed414bc1688af5999f7331d10e841fd068fc
Cris M <cristian.montenegro.p@gmail.com>
2022-10-04 22:59:51 -0300

    changed naming convention a bit, and organized returned array of matches structure

--- moduleFn/htmlFileIndexer.js
+++ moduleFn/htmlFileIndexer.js
@@ -1,12 +1,45 @@
 // index the html file into an array of strings, by separating every element by the symbols <>
-import { fileToEdit } from './index.js';
+// import { readFile } from './index.js';
+import fs from 'fs';
 
-export function indexFile({ elementStart, elementEnd }) {
+export async function indexFile(
+  fileInfo
+  // fileContent, oldElementBegining, oldElementEnd, selfEnclosed
+) {
+  console.log('indexFile() started executing');
+  // console.log('fileInfo in htmlFileIndexer: ', fileInfo);
   let result = {};
+  let propertiesArray = [];
+  let allElementsArr = [];
+  let allFileElementsRgxPattern = '';
+  let elementRgx;
+  let fileIndexingArrObj = {};
+  let searchAllElementsArr;
 
-  console.log();
-  // const imgRgx = /<img.+?\/>/gims;
-  const elementRgx = new Regex(`${elementStart}.+?${elementEnd}`, 'gims');
+  if (fileInfo.selfEnclosed) {
+    //  <img  //  />
+    elementRgx = new RegExp(
+      `${fileInfo.oldElementBegining}.+?${fileInfo.oldElementEnd.replaceAll(
+        '/',
+        '\\/'
+      )}`,
+      'gims'
+    );
+  } else {
+    // <div    > ... </div>
+    elementRgx = new RegExp(`${fileInfo.oldElementBegining}.+?>`, 'gims');
+  }
+
+  // console.log(
+  //   'oldElementBegining: ',
+  //   fileInfo.oldElementBegining,
+  //   'oldoldElementEnd: ',
+  //   fileInfo.oldElementEnd
+  // );
+
+  // oldElementEnd = oldElementEnd.replaceAll('/', '\\/');
+  // const allFileElementsRgx = new RegExp(`<\\s|\\w.+?>`, 'gims');
+  const allFileElementsRgx = new RegExp(`<.+?>`, 'gims');
   const classRgx = /(class="|').+?(?="|')/ims;
   const altRgx = /(alt="|').+?(?="|')/ims;
   const srcRgx = /(src=("|')).+?(?="|')/ims;
@@ -19,36 +52,140 @@ export function indexFile({ elementStart, elementEnd }) {
   const loadingRgx = /(loading="|').+?(?="|')/ims;
   const crossoriginRgx = /(crossorigin="|').+?(?="|')/ims;
 
-  let fileIndexingArrObj = [...fileToEdit.matchAll(elementRgx)];
+  searchAllElementsArr = [
+    ...fileInfo.inputFileContent.matchAll(allFileElementsRgx),
+  ];
 
-  fileIndexingArrObj = fileIndexingArrObj.map((x) => {
+  for (let i = 0; i < searchAllElementsArr.length; i++) {
+    allElementsArr.push(searchAllElementsArr[i][0]);
+  }
+  // for (let i = 0; i < searchAllElementsArr.length; i++) {
+  //   // console.dir(searchAllElementsArr[i][0]);
+  //   if (/<!--.+?-->/i.test(searchAllElementsArr[i][0]) === false) {
+  //     // searchAllElementsArrFiltered.push(searchAllElementsArr[i][0]);
+  //     searchAllElementsArrFiltered += searchAllElementsArr[i][0];
+  //   }
+  // }
+
+  // console.log('searchAllElementsArr: ');
+  // console.table(
+  //   searchAllElementsArr.map((x) => {
+  //     return x[0];
+  //   })
+  // );
+
+  // console.log('elementRgx: ', elementRgx);
+  // let fileIndexingArrObj = [...fileContent.matchAll(elementRgx)];
+  // console.log('searchAllElementsArr Length: ', searchAllElementsArr.length);
+
+  // console.table(searchAllElementsArrFiltered);
+  // fileIndexingArrObj = [...searchAllElementsArr.matchAll(elementRgx)];
+  // console.log('fileIndexingArrObj: ');
+  // console.dir(fileIndexingArrObj);
+  // fileIndexingArrObj = searchAllElementsArr.matchAll(elementRgx);
+  // searchAllElementsArr.map((x) => {});
+
+  fileIndexingArrObj = searchAllElementsArr.map((x, i) => {
+    let elMatch = x[0].match(elementRgx);
+    if (elMatch === null) {
+      return null;
+    }
+    // console.log('element Match: ');
+    // console.dir(elMatch);
+
     result = {
-      alt: altRgx.test(x[0]) ? x[0].match(altRgx)[0] + '"' : null,
-      crossorigin: crossoriginRgx.test(x[0])
-        ? x[0].match(crossoriginRgx)[0] + '"'
-        : null,
-      class: classRgx.test(x[0]) ? x[0].match(classRgx)[0] + '"' : null,
-      dataSrc: dataSrcRgx.test(x[0])
-        ? 'data-src="' + x[0].match(dataSrcRgx)[0] + '"'
-        : null,
-      height: heightRgx.test(x[0]) ? x[0].match(widthRgx)[0] + '"' : null,
-      id: idRgx.test(x[0]) ? `${x[0].match(idRgx)[0]}"` : null,
-      loading: loadingRgx.test(x[0]) ? x[0].match(loadingRgx)[0] + '"' : null,
-      sizes: sizesRgx.test(x[0]) ? x[0].match(sizesRgx)[0] + '"' : null,
+      alt: altRgx.test(elMatch[0]) ? elMatch[0].match(altRgx)[0] + '"' : false,
+      crossorigin: crossoriginRgx.test(elMatch[0])
+        ? elMatch[0].match(crossoriginRgx)[0] + '"'
+        : false,
+      class: classRgx.test(elMatch[0])
+        ? elMatch[0].match(classRgx)[0] + '"'
+        : false,
+      dataSrc: dataSrcRgx.test(elMatch[0])
+        ? elMatch[0].match(dataSrcRgx)[0] + '"'
+        : false,
+      height: heightRgx.test(elMatch[0])
+        ? elMatch[0].match(widthRgx)[0] + '"'
+        : false,
+      id: idRgx.test(elMatch[0]) ? `${elMatch[0].match(idRgx)[0]}"` : false,
+      loading: loadingRgx.test(elMatch[0])
+        ? elMatch[0].match(loadingRgx)[0] + '"'
+        : false,
+      sizes: sizesRgx.test(elMatch[0])
+        ? elMatch[0].match(sizesRgx)[0] + '"'
+        : false,
       src:
-        srcRgx.test(x[0]) && !dataSrcRgx.test(x[0])
-          ? 'src="' + x[0].match(srcRgx)[0] + '"'
-          : null,
-      srcset: srcsetRgx.test(x[0]) ? x[0].match(srcsetRgx)[0] : null,
-      width: widthRgx.test(x[0]) ? x[0].match(widthRgx)[0] + '"' : null,
-      elString: x[0],
-      verifyString: fileToEdit.substring(x.index, x.index + x[0].length),
+        srcRgx.test(elMatch[0]) && !dataSrcRgx.test(elMatch[0])
+          ? elMatch[0].match(srcRgx)[0] + '"'
+          : false,
+      srcset: srcsetRgx.test(elMatch[0])
+        ? elMatch[0].match(srcsetRgx)[0]
+        : false,
+      width: widthRgx.test(elMatch[0])
+        ? elMatch[0].match(widthRgx)[0] + '"'
+        : false,
+      elString: elMatch[0],
+      verifyString: fileInfo.inputFileContent.substring(
+        x.index,
+        x.index + elMatch[0].length
+      ),
+      arrIndexPosition: i,
       indexStart: x.index,
-      indexEnd: x.index + x[0].length,
+      length: elMatch[0].length,
     };
+    propertiesArray = [
+      result.alt,
+      result.crossorigin,
+      result.class,
+      result.dataSrc,
+      result.height,
+      result.id,
+      result.loading,
+      result.sizes,
+      result.src,
+      result.srcset,
+      result.width,
+    ];
 
-    return result;
+    return { result, propertiesArray };
   });
 
-  return result;
+  let consoleResult1 = [];
+  let consoleResult2 = [];
+  // for (let i = 0; i < 7; i++) {
+  //   consoleResult1 += fileIndexingArrObj[i];
+  // }
+  // console.dir(consoleResult1);
+  // console.log(fileIndexingArrObj.length);
+  for (let i = 0; i < fileIndexingArrObj.length; i++) {
+    // console.log('i here is: ', i);
+
+    // console.log('fileIndexArOb[i]: ', fileIndexingArrObj[i]);
+
+    // console.log('fileIndexingArrObj.length', fileIndexingArrObj.length);
+    if (fileIndexingArrObj[i] === null) {
+      let deletedElement = fileIndexingArrObj.splice(i, 1);
+      // console.log('deletedElememt: ', deletedElement);
+      // console.log('if Triggered!');
+      // console.log('fileIndexingArrObj.length', fileIndexingArrObj.length);
+      // fileIndexingArrObj -= fileIndexingArrObj[i];
+      i--;
+      // console.log('i: ', i);
+    }
+    // console.log('fileIndexArOb[i] After: ');
+    // console.dir(fileIndexingArrObj[i]);
+  }
+
+  // for (let i = 0; i < 7; i++) {
+  //   consoleResult2 += fileIndexingArrObj[i];
+  // }
+
+  // console.log(
+  //   'result is : ----->!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+  // );
+  // console.dir(consoleResult2);
+  console.log('indexFile() finished executing');
+  // console.log('indexFile() final Result: ');
+  // console.dir(fileIndexingArrObj);
+  return { fileIndexingArrObj, allElementsArr };
 }
