a917bb6724e432e2c2eb90da3b84216f504e2eae
Cris M <cristian.montenegro.p@gmail.com>
2022-10-21 17:08:08 -0300

    re-implemented inquierer prompts and cleaned file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
   readFileContent,
   replaceElement,
 } from './moduleFn/index.js';
+import util from 'util';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,37 +15,12 @@ let fileInfo;
 let inputFileName;
 // let inputFileNamesArr;
 
-var functionVar = {
-  exit: function () {
-    // inquirer
-    //   .prompt([
-    //     {
-    //       type: 'confirm',
-    //       message: 'do you wish to quit?',
-    //       name: 'quit',
-    //     },
-    //   ])
-    //   .then(({ quit }) => {
-    //     if (quit) {
-    //       return;
-    //     } else {
-    //       functionVar.greetings();
-    //     }
-    //   });
-  },
-  readAllFileNamesArr: async function () {
-    // inputFileNamesArr = await readFiles(inputDirPath);
-    // return inputFileNamesArr;
-    // return await readFiles(inputDirPath)
-  },
+var fnObj = {
   structureFileNameData: async function (inputFileName) {
     const inputFilePath = path.join(inputDirPath, inputFileName);
     // console.log('readFilesResponse: ', readFilesResponse);
-
     // console.log('inputFilePath: ', inputFilePath);
-
     // console.log('file content: ', readFileContentResponse);
-
     fileInfo = {
       inputDirPath: inputDirPath,
       inputDirRelativePath: './insert-file-here',
@@ -73,9 +49,9 @@ var functionVar = {
       .then(async ({ isFileGood }) => {
         if (!isFileGood) {
           console.log('oh no!');
-          setTimeout(() => functionVar.exit(), 1000);
+          setTimeout(() => fnObj.exit(), 1000);
         } else {
-          functionVar.inputFileSelection();
+          fnObj.inputFileSelection();
         }
       });
   },
@@ -92,96 +68,99 @@ var functionVar = {
       .then(async ({ fileSelection }) => {
         console.log('fileSelection: ', fileSelection);
         inputFileName = fileSelection;
-        fileInfo = await functionVar.structureFileNameData(fileSelection);
-        functionVar.elementsToReplace();
+        fileInfo = await fnObj.structureFileNameData(fileSelection);
+        fnObj.elementsToReplace();
       });
   },
   elementsToReplace: async function () {
-    //replacing prompt for information, disable later
+    let { oldElement, newElement, selfEnclosed } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        message: 'is the element self closing? eg: <img />',
+        name: 'selfEnclosed',
+      },
+      {
+        type: 'input',
+        message:
+          'please write down the element that needs to be replaced, and an "*" where attributes go, all existing attributes will be kept',
+        name: 'oldElement',
+      },
+      {
+        type: 'input',
+        message:
+          'please write down the replacement html element, and an "*" where attributes go, all existing attributes will be kept',
+        name: 'newElement',
+      },
+    ]);
+
+    // console.log('final answer in Inquirer: ');
+    // console.table(answer);
+    // fnObj.goodbye();
+    // });
+
+    console.log(oldElement, newElement, selfEnclosed);
     let { replaceElementSuccess, writeOnFileResponse } = await replaceElement({
-      oldElement: '<img * />',
-      newElement: '<yikes * />',
-      selfEnclosed: true,
+      oldElement,
+      newElement,
+      selfEnclosed,
+      //replacing prompt for information, disable later
+      // oldElement: '<img * />',
+      // newElement: '<yikes * />',
+      // selfEnclosed: true,
       ...fileInfo,
     });
 
-    if (replaceElementSuccess) {
+    if (replaceElementSuccess && writeOnFileResponse) {
       console.log('Great!, all went along nicely');
-      functionVar.goodbye(writeOnFileResponse);
+      fnObj.exit({ replaceElementSuccess, writeOnFileResponse });
     } else {
-      functionVar.error(error);
+      fnObj.error();
     }
   },
 
-  //   await inquirer
-  //     .prompt([
-  //       {
-  //         type: 'confirm',
-  //         message: 'is the element self closing? eg: <img />',
-  //         name: 'selfEnclosed',
-  //       },
-  //       {
-  //         type: 'input',
-  //         message:
-  //           'please write down the element that needs to be replaced, and an "*" where attributes go, all existing attributes will be kept',
-  //         name: 'oldHtmlElement',
-  //       },
-  //       {
-  //         type: 'input',
-  //         message:
-  //           'please write down the replacement html element, and an "*" where attributes go, all existing attributes will be kept',
-  //         name: 'newHtmlElement',
-  //       },
-  //     ])
-  //     .then(
-  //       async ({
-  //         isFileGood,
-  //         oldHtmlElement,
-  //         newHtmlElement,
-  //         selfEnclosed,
-  //       }) => {
-  //         isFileGood
-  //           ? (
+  // goodbye: async function ({ replaceElementSuccess, writeOnFileResponse }) {
+  //   if (replaceElementSuccess && writeOnFileResponse) {
+  //     let outFileContent = await readFileContent(fileInfo.outputFilePath);
+  //     inquirer
+  //       .prompt([
+  //         {
+  //           type: 'confirm',
 
-  //             answer = await replaceElement(
-  //               oldHtmlElement,
-  //               newHtmlElement,
-  //               selfEnclosed
-  //             )
+  //           message: `how is this looking?: ${console.dir(outFileContent)}`,
+  //           name: 'confirmOutput',
+  //         },
+  //       ])
+  //       .then(({ confirmOutput }) => {
+  //         confirmOutput ? console.log('Done!, enjoy.') : fnObj.exit();
+  //       })
+  //       .catch((err) => {
+  //         console.err('Oops!, something went wrong');
+  //         throw err;
+  //       });
+  //   }
+  // },
 
-  //             )
-  //           : console.err(
-  //               'you need to paste the content into the variable!',
-  //               prompt.complete()
-  //             );
-
-  //         // console.log('final answer in Inquirer: ');
-  //         // console.table(answer);
-  //         functionVar.goodbye();
-  //       }
-  //     );
-
-  goodbye: async function (editedFileContent) {
+  error: function (error) {
+    console.log(`Oh no!, something went wrong, here is the error: ${error} `);
+    fnObj.exit();
+  },
+  exit: function () {
     inquirer
       .prompt([
         {
           type: 'confirm',
-          message: `how is this looking?: ${console.dir(editedFileContent)}`,
-          name: 'confirmOutput',
+          message: 'do you wish to quit?',
+          name: 'quit',
         },
       ])
-      .then(({ confirmOutput }) => {
-        confirmOutput ? console.log('Done!, enjoy.') : functionVar.exit();
-      })
-      .catch((err) => {
-        console.log('Oops!, something went wrong');
-        throw err;
+      .then(({ quit }) => {
+        if (quit) {
+          return;
+        } else {
+          fnObj.greetings();
+        }
       });
-  },
-  error: function (error) {
-    console.log(`Oh no!, something went wrong, here is the error: ${error} `);
-    functionVar.exit();
   },
 };
 
-functionVar.greetings();
+fnObj.greetings();
