83604a8ba2ad11994d02115107e1321af3749183
Cris M <cristian.montenegro.p@gmail.com>
2022-10-21 14:18:12 -0300

    removed deprecated dependencies

--- moduleFn/index.js
+++ moduleFn/index.js
@@ -1,6 +1,4 @@
 export { indexFile } from './htmlFileIndexer.js';
-// export { fileToEdit } from '../insert-file-here/file.js';
 export { replaceElement } from './elementReplacer.js';
 export { writeOnFile } from './writeOnFile.js';
 export { readFiles, readFileContent } from './readFile.js';
-// export { esitedFile } from './edited-file/hello.js';
