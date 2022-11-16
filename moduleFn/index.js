c5e3d6a0a90e41f45fccd39c8ac65299e9971431
Cris M <cristian.montenegro.p@gmail.com>
2022-11-16 17:03:41 -0300

    added new function to elementReplacer.js

--- moduleFn/index.js
+++ moduleFn/index.js
@@ -1,4 +1,4 @@
 export { indexFile } from './htmlFileIndexer.js';
-export { replaceElement } from './elementReplacer.js';
+export { replaceElement, replaceImageToPicture } from './elementReplacer.js';
 export { writeOnFile } from './writeOnFile.js';
 export { readFiles, readFileContent } from './readFile.js';
