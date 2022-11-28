a08e45f5f3623223d3e62d52cceed21c16476cf4
unknown <cristian.montenegro.p@gmail.com>
2022-11-28 16:58:17 -0300

    modified h

--- moduleFn/elementReplacer.js
+++ moduleFn/elementReplacer.js
@@ -60,10 +60,10 @@ export async function replaceElement(fileInfo) {
 export async function replaceImageToPicture(fileInfo) {
   let newElementBegining = '<picture';
   let newElementEnd = '>';
-  let { imageTypeSelection, breakpointSelection } = fileInfo;
+  let { imageTypeSelection, breakPointSelection } = fileInfo;
 
   //split the element to be replaced into 2 separate strings
-  const [oldElementBegining, oldElementEnd] = fileInfo.oldElement
+  const [oldElementBegining, oldElementEnd] = fileInfo.element_to_replace
     .replaceAll(' ', '')
     .split('*');
 
@@ -79,68 +79,8 @@ export async function replaceImageToPicture(fileInfo) {
   let { fileIndexingArrObj, allElementsArr } = indexFile(fileInfo);
 
   //
-  console.log('fileIndexingArrObj: ');
-  console.log(fileIndexingArrObj);
-  // {
-  //   result: {
-  //     alt: false,
-  //     crossorigin: false,
-  //     class: 'class="lazy size-1"',
-  //     dataSrc: 'data-src="./src/img/Floral_Pattern_6000x6000.jpg"',
-  //     height: false,
-  //     id: false,
-  //     loading: false,
-  //     sizes: false,
-  //     srcset: false,
-  //     src: false,
-  //     width: false,
-  //     elString: '<img\n' +
-  //       '        class="lazy size-1"\n' +
-  //       '        data-src="./src/img/Floral_Pattern_6000x6000.jpg"\n' +
-  //       '      />',
-  //     verifyString: '<img\n' +
-  //       '        class="lazy size-1"\n' +
-  //       '        data-src="./src/img/Floral_Pattern_6000x6000.jpg"\n' +
-  //       '      />',
-  //     arrIndexPosition: 220,
-  //     indexStart: 14624,
-  //     length: 99,
-  //     imageSourcePath: './src/img/Floral_Pattern_6000x6000.jpg',
-  //     imageFileExtension: 'jpg'
-  //   },
-  //   propertiesArray: [
-  //     false,
-  //     false,
-  //     'class="lazy size-1"',
-  //     'data-src="./src/img/Floral_Pattern_6000x6000.jpg"',
-  //     false,
-  //     false,
-  //     false,
-  //     false,
-  //     false,
-  //     false,
-  //     false
-  //   ]
-  // }
-
-  // fileInfo = {
-  //   oldElement: oldElement,
-  //   imageTypeSelection: imageTypeSelection,
-  //   breakpointSelection: breakpointSelection,
-  //   inputDirPath: inputDirPath,
-  //   inputDirRelativePath: './insert-file-here',
-  //   inputFileName: inputFileName,
-  //   inputFilePath: inputFilePath,
-  //   outputFileName: inputFileName,
-  //   outputFilePath: path.join(__dirname, './edited-file', inputFileName),
-  //   outputDirPath: path.join(__dirname, './edited-file'),
-  //   outputRelativeDirPath: './edited-file',
-  //   outputRelativeFilePath: path.join('./edited-file', inputFileName),
-  //   inputFileContent: await readFileContent(inputFilePath),
-  // };
-
-  // imageTypeSelection: ['avif', 'webp', 'img']
-  //breakpointSelection: ['0-480px', '481-768px', '769-1279px', '1280+px']
+  // console.log('fileIndexingArrObj: ');
+  // console.log(fileIndexingArrObj);
 
   // console.log('allElementsArr: ');
   // console.dir(allElementsArr);
@@ -163,64 +103,34 @@ export async function replaceImageToPicture(fileInfo) {
         let sourceElementSizes = '';
         // x = 'avif' | 'webp' | 'jpeg'
 
-        // Map looper inside map looper that loops through breakpointselection to configure
+        // Map looper inside map looper that loops through breakPointSelection to configure
         // "sizes and srcset attribute" in each <source>
-        breakpointSelection.map((y) => {
-          if (y === '0-480px') {
-            sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-xs',
-              extname: '.' + x,
-            })} 480w, `;
-            sourceElementSizes += '(max-width: 480px) 480px, ';
-          }
-          if (y === '481-768px') {
-            sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-sm',
-              extname: '.' + x,
-            })} 768w, `;
-            sourceElementSizes += '(max-width: 768px) 768px, ';
-          }
-          if (y === '769-1279px') {
-            sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-md',
-              extname: '.' + x,
-            })} 1279w, `;
-            sourceElementSizes += '(max-width: 1279px) 1279px, ';
-          }
-          if (y === '1280+px') {
-            sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-lg',
-              extname: '.' + x,
-            })} 1280w, `;
-            sourceElementSizes += '(min-width: 1280px) 1280px, ';
-          }
+        breakPointSelection.map((y) => {
+          // {
+          //     breakpoint_start: 0,
+          //     breakpoint_end: 480,
+          //     image_width: 480,
+          //     suffix: '-xs',
+          //   }
 
-          if (y === '0-767px') {
+          if (y.breakpoint_end) {
             sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-sm',
+              suffix: y.suffix,
               extname: '.' + x,
-            })} 767w, `;
-            sourceElementSizes += '(max-width: 767px) 767px, ';
-          }
-          if (y === '768-1023px') {
+            })} ${y.breakpoint_end}w, `;
+            sourceElementSizes += `(max-width: ${y.breakpoint_end}px)  ${y.breakpoint_end}px, `;
+          } else {
             sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-md',
+              suffix: y.suffix,
               extname: '.' + x,
-            })} 1023w, `;
-            sourceElementSizes += '(max-width: 1023px) 1023px, ';
-          }
-          if (y === '1024+px') {
-            sourceElementSrcset += `${rename(result.imageSourcePath, {
-              suffix: '-lg',
-              extname: '.' + x,
-            })} 1024w, `;
-            sourceElementSizes += '(min-width: 1024px) 1024px, ';
+            })} ${y.breakpoint_start}w, `;
+            sourceElementSizes += `(min-width: ${y.breakpoint_start}px) ${y.breakpoint_start}px, `;
           }
         });
         allSourcesString += `<source type="image/${x}" sizes="${sourceElementSizes}" srcset="${sourceElementSrcset}" /> \n`;
       });
 
-      // This appends the newly constructed element and replacing it for the old one
+      // This appends the newly constructed element and replaces it for the old one
       // in AllElementsArr
       let deletedString = allElementsArr.splice(
         result.arrIndexPosition,
@@ -240,9 +150,70 @@ export async function replaceImageToPicture(fileInfo) {
     return { replaceElementSuccess: false, writeOnFileResponse: null };
   });
 
-  console.log('replaceElement() finished executing');
+  // console.log('replaceElement() finished executing');
   return {
     replaceElementSuccess: true,
     writeOnFileResponse: writeOnFileResponse,
   };
 }
+
+// {
+//   result: {
+//     alt: false,
+//     crossorigin: false,
+//     class: 'class="lazy size-1"',
+//     dataSrc: 'data-src="./src/img/Floral_Pattern_6000x6000.jpg"',
+//     height: false,
+//     id: false,
+//     loading: false,
+//     sizes: false,
+//     srcset: false,
+//     src: false,
+//     width: false,
+//     elString: '<img\n' +
+//       '        class="lazy size-1"\n' +
+//       '        data-src="./src/img/Floral_Pattern_6000x6000.jpg"\n' +
+//       '      />',
+//     verifyString: '<img\n' +
+//       '        class="lazy size-1"\n' +
+//       '        data-src="./src/img/Floral_Pattern_6000x6000.jpg"\n' +
+//       '      />',
+//     arrIndexPosition: 220,
+//     indexStart: 14624,
+//     length: 99,
+//     imageSourcePath: './src/img/Floral_Pattern_6000x6000.jpg',
+//     imageFileExtension: 'jpg'
+//   },
+//   propertiesArray: [
+//     false,
+//     false,
+//     'class="lazy size-1"',
+//     'data-src="./src/img/Floral_Pattern_6000x6000.jpg"',
+//     false,
+//     false,
+//     false,
+//     false,
+//     false,
+//     false,
+//     false
+//   ]
+// }
+
+// fileInfo = {
+//   oldElement: oldElement,
+//   imageTypeSelection: imageTypeSelection,
+//   breakPointSelection: breakPointSelection,
+//   inputDirPath: inputDirPath,
+//   inputDirRelativePath: './insert-file-here',
+//   inputFileName: inputFileName,
+//   inputFilePath: inputFilePath,
+//   outputFileName: inputFileName,
+//   outputFilePath: path.join(__dirname, './edited-file', inputFileName),
+//   outputDirPath: path.join(__dirname, './edited-file'),
+//   outputRelativeDirPath: './edited-file',
+//   outputRelativeFilePath: path.join('./edited-file', inputFileName),
+//   inputFileContent: await readFileContent(inputFilePath),
+// };
+
+// imageTypeSelection: ['avif', 'webp', 'img']
+//breakPointSelection: ['0-480px', '481-768px', '769-1279px', '1280+px']
