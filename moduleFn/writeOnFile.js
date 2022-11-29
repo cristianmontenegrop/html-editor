56158f1667b64839d6a6863294dc08ddd8f07e82
unknown <cristian.montenegro.p@gmail.com>
2022-11-29 11:39:56 -0300

    cleaning code and fixing catch statements

--- moduleFn/writeOnFile.js
+++ moduleFn/writeOnFile.js
@@ -3,7 +3,6 @@ import util from 'util';
 import inquirer from 'inquirer';
 
 export async function writeOnFile(fileInfo) {
-  // console.log('function writeFile Executed!!');
   fileInfo.editedFileContent = fileInfo.editedFileContent.join('');
 
   const promisify = util.promisify;
@@ -13,55 +12,49 @@ export async function writeOnFile(fileInfo) {
 
   let fsFnObj = {
     access: function () {
-      console.log('access!');
       try {
         let fileExists = fs.existsSync(fileInfo.outputFilePath);
         return fileExists;
       } catch (err) {
-        console.err(err);
+        console.error(err);
       }
     },
     copy: async function () {
-      console.log('copy!');
       try {
         const pathBegining =
           fileInfo.outputRelativeFilePath.split(/\.[0-9a-z]+$/i);
         const pathEnd = fileInfo.outputRelativeFilePath.split(/^.+\./i);
         const copyPath =
           pathBegining[0] + 'copy.' + pathEnd[pathEnd.length - 1];
-        console.log('copyPath: ', copyPath);
         return await fsCopyPromise(
           fileInfo.outputRelativeFilePath,
           copyPath,
           fs.constants.COPYFILE_FICLONE
         );
       } catch (err) {
-        console.err('Error!', err);
+        console.error('Error!', err);
       }
     },
     unLink: async function (path) {
-      console.log('unlink!');
       try {
         return await fsUnlinkPromise(path);
       } catch (err) {
-        console.err('Error!', err);
+        console.error('Error!', err);
       }
     },
     append: async function () {
-      console.log('append!');
       try {
         return await fsAppendPromise(
           fileInfo.outputRelativeFilePath,
           fileInfo.editedFileContent
         );
-      } catch {
-        (err) => console.err('Error!', err);
+      } catch (err) {
+        console.error('Error!', err);
       }
     },
   };
 
   let x = fsFnObj.access();
-  console.log('x: ', x);
   if (x) {
     let { confirmCopy } = await inquirer.prompt([
       {
@@ -73,12 +66,10 @@ export async function writeOnFile(fileInfo) {
     ]);
 
     let copyRes;
-    
+
     if (confirmCopy) {
       copyRes = await fsFnObj.copy();
-      console.log('copyRes: ', copyRes);
       const fileUnlink = await fsFnObj.unLink(fileInfo.outputRelativeFilePath);
-      console.log('fileUnlink: ', fileUnlink);
       const appendFile = await fsFnObj.append();
       if (appendFile === undefined) {
         return fsFnObj.access(fileInfo.outputRelativeFilePath);
@@ -87,23 +78,20 @@ export async function writeOnFile(fileInfo) {
       }
     } else {
       //  disregard existing file, replace it
-      console.log('do not create copy, replace!!');
       try {
         const fileUnlink = await fsFnObj.unLink(
           fileInfo.outputRelativeFilePath
         );
-        console.log('fileUnlink: ', fileUnlink);
         const appendFile = await fsFnObj.append();
         if (appendFile === undefined) {
           return fsFnObj.access(fileInfo.outputRelativeFilePath);
         } else {
           return false;
         }
-      } catch {
-        (err) => console.log('Error!', err);
+      } catch (err) {
+        console.error('Error!', err);
       }
     }
-
   } else {
     try {
       const appendFile = await fsFnObj.append();
@@ -112,8 +100,8 @@ export async function writeOnFile(fileInfo) {
       } else {
         return false;
       }
-    } catch {
-      (err) => console.log('Error!', err);
+    } catch (err) {
+      console.error('Error!', err);
     }
   }
 }
